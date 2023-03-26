350962509b8af6fef96f6ad9ecb38206074e5108
Ernesto Salvador Pisano <pisano5@hotmail.com>
2023-03-26 17:59:57 -0300

    feat: add skills to portfolio

---
+++ src/app/components/skills/new-skill.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Skills } from 'src/app/model/skills';
+import { SkillService } from 'src/app/service/skills-service.service';
+
+@Component({
+  selector: 'app-new-skill',
+  templateUrl: './new-skill.component.html',
+  styleUrls: ['./new-skill.component.css']
+})
+export class NewSkillComponent implements OnInit {
+  name: string;
+  percentage: number;
+
+  constructor(private skillService: SkillService, private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  onCreate(): void{
+    const skill = new Skills(this.name, this.percentage);
+    this.skillService.save(skill).subscribe(
+      data => {
+        alert("Skill creada correctamente");
+        this.router.navigate(['']);
+      }, err =>{
+        alert("Fallo al añadir la skill");
+        this.router.navigate(['']);
+      }
+    )
+  }
+}
--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Skills } from 'src/app/model/skills';
+import { SkillService } from 'src/app/service/skills-service.service';
+import { TokenService } from 'src/app/service/token.service';
 
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
   styleUrls: ['./skills.component.css']
 })
-export class SkillsComponent {
+export class SkillsComponent implements OnInit {
+  skills: Skills[] = [];
 
+  constructor(private skillsService: SkillService, private tokenService: TokenService) { }
+  isLogged = false;
+  
+  ngOnInit(): void {
+    this.loadSkills();
+    if(this.tokenService.getToken()){
+      this.isLogged = true;
+    } else {
+      this.isLogged = false;
+    }
+  }
+
+  loadSkills(): void{
+    this.skillsService.list().subscribe(
+      data => {
+        this.skills = data;
+      }
+    )
+  }
+
+  delete(id: number){
+    if(id != undefined){
+      this.skillsService.delete(id).subscribe(
+        data => {
+          this.loadSkills();
+        }, err => {
+          alert("No se pudo borrar la skill");
+        }
+      )
+    }
+  }
 }
